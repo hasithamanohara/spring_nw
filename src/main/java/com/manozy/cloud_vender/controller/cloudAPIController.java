07e90593c095e743c18cc47d6b6831ac720e1d4e
hasithauom <manoharauh.20@uom.lk>
2023-11-07 01:26:35 +0530

    get

--- src/main/java/com/manozy/cloud_vender/controller/cloudAPIController.java
+++ src/main/java/com/manozy/cloud_vender/controller/cloudAPIController.java
@@ -1,16 +1,22 @@
 package com.manozy.cloud_vender.controller;
 
 import com.manozy.cloud_vender.model.cloud_vendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/cloudvender")
 public class cloudAPIController {
+    cloud_vendor Cloudvendor;
     @GetMapping("/getVendor")
     public cloud_vendor getCloudVendor(String vendorId){
-        return new cloud_vendor("c","vendor1",
-                   "address1","071");
+        return Cloudvendor;
+//                new cloud_vendor;
+//                ("c","vendor1",
+//                   "address1","071");
+    }
+    @PostMapping
+    public String createCloudVendor(@RequestBody cloud_vendor Cloudvendor){
+        this.Cloudvendor = Cloudvendor;
+        return "succes";
     }
 }
